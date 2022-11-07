e92e07751ca0bb1c2e264ab334f04f58a5dc8c59
Yoav Levenson <yoav@practis.co.il>
2022-11-07 10:45:40 +0000

    finish preparing exercise

--- FooBoo.Tests.Unit/FooBooLogicTests.cs
+++ FooBoo.Tests.Unit/FooBooLogicTests.cs
@@ -25,46 +25,5 @@ namespace FooBoo.Tests.Unit
             // Assert
             Assert.AreEqual(expected, actual);
         }
-
-        [Test]
-        public void Analyze_DividesBy3_ValueOfFoo()
-        {
-            // Arrange
-            string expected = "Foo";
-
-            // Act
-            string actual = fooBooLogic.Analyze(3);
-
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        
-        [Test]
-        public void Analyze_DividesBy5_ValueOfBoo()
-        {
-            // Arrange
-            string expected = "Boo";
-
-            // Act
-            string actual = fooBooLogic.Analyze(5);
-
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
-
-        
-        [Test]
-        public void Analyze_DividesBy3And5_ValueOfFooBoo()
-        {
-            // Arrange
-            string expected = "FooBoo";
-
-            // Act
-            string actual = fooBooLogic.Analyze(15);
-
-            // Assert
-            Assert.AreEqual(expected, actual);
-        }
     }
 }
